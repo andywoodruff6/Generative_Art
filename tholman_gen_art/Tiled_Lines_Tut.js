2335a9ab5f5f806e0c82f30b8bbd3c05d2c36377
Andrew Woodruff <andywoodruff6@gmail.com>
2021-08-07 16:21:22 -0400

    fixed the plot size

--- tholman_gen_art/Tiled_Lines_Tut.js
+++ tholman_gen_art/Tiled_Lines_Tut.js
@@ -1,22 +1,32 @@
 var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
 
-var size = window.innerWidth;
-var dpr = window.devicePixelRatio;
-canvas.width = size * dpr;
-canvas.height = size * dpr;
-context.scale(dpr, dpr);
+// var size = window.innerWidth;
+var step = 80;
+// var dpr = window.devicePixelRatio;
+var number = 320;
+
+canvas.width = number ;
+canvas.height = number ;
+context.scale(2, 2);
 
 context.lineCap = 'square';
 context.lineWidth = 2;
 
 function draw(x, y, width, height) {
+  var leftToRight = Math.random() >= 0.5;
+
+  if(leftToRight) {
     context.moveTo(x, y);
-    context.lineTo(x + width, y + height);   
+    context.lineTo(x + width, y + height);    
+  } else {
+    context.moveTo(x + width, y);
+    context.lineTo(x, y + height);
+  }
     context.stroke();
 } //This function takes an init x,y location and then draws a line from there 
   //The line is then drawn to the entire width and height
   
-draw(0, 0, size, size);
+draw(0, 0, number, number);
 
 //TEST
