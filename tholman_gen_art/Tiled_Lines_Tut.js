650a1b495a56fe8fb4bca28363c05ba2087542eb
Andrew Woodruff <andywoodruff6@gmail.com>
2021-08-07 16:24:52 -0400

    finished tutorial

--- tholman_gen_art/Tiled_Lines_Tut.js
+++ tholman_gen_art/Tiled_Lines_Tut.js
@@ -2,7 +2,7 @@ var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
 
 // var size = window.innerWidth;
-var step = 80;
+var step = 20;
 // var dpr = window.devicePixelRatio;
 var number = 320;
 
@@ -27,6 +27,14 @@ function draw(x, y, width, height) {
 } //This function takes an init x,y location and then draws a line from there 
   //The line is then drawn to the entire width and height
   
+for(var x = 0; x < number; x += step) {
+  for(var y = 0; y < number; y+= step) {
+    draw(x, y, step, step);    
+  }
+}
+  
+
+
 draw(0, 0, number, number);
 
 //TEST
