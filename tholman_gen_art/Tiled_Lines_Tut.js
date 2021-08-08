e2d99c5ea0e12062e083d31085ceddcef67733c4
Andrew Woodruff <andywoodruff6@gmail.com>
2021-08-07 21:43:39 -0400

    added comments

--- tholman_gen_art/Tiled_Lines_Tut.js
+++ tholman_gen_art/Tiled_Lines_Tut.js
@@ -1,13 +1,15 @@
-var canvas = document.querySelector('canvas');
+var canvas = document.querySelector('canvas'); 
+  // This matches with the first time 'canvas' appears in the html document
 var context = canvas.getContext('2d');
+  // leads to the creation of a CanvasRenderingContext2D object represnting a 2d render
 
 // var size = window.innerWidth;
 var step = 20;
 // var dpr = window.devicePixelRatio;
 var number = 320;
 
-canvas.width = number ;
-canvas.height = number ;
+canvas.width = number;
+canvas.height = number;
 context.scale(2, 2);
 
 context.lineCap = 'square';
@@ -15,7 +17,8 @@ context.lineWidth = 2;
 
 function draw(x, y, width, height) {
   var leftToRight = Math.random() >= 0.5;
-
+  // leftToRight returns a Bool 
+  // if true then the first argument, else the second
   if(leftToRight) {
     context.moveTo(x, y);
     context.lineTo(x + width, y + height);    
@@ -24,17 +27,12 @@ function draw(x, y, width, height) {
     context.lineTo(x, y + height);
   }
     context.stroke();
-} //This function takes an init x,y location and then draws a line from there 
-  //The line is then drawn to the entire width and height
+} //This function takes an initial x,y location and then draws a line from there 
+  //The line is then drawn to the length of width and height
   
 for(var x = 0; x < number; x += step) {
   for(var y = 0; y < number; y+= step) {
     draw(x, y, step, step);    
   }
 }
-  
-
-
 draw(0, 0, number, number);
-
-//TEST
